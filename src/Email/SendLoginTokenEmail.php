eca1245135c011838925796d78921745f3efac65
Bob Bloom <info@southlasalle.com>
2016-04-28 15:55:28 -0400

    Modify namespaces and paths. #2

--- src/Email/SendLoginTokenEmail.php
+++ src/Email/SendLoginTokenEmail.php
@@ -1,37 +1,34 @@
 <?php
 
-namespace Lasallecrm\Lasallecrmemail\Logintoken;
+namespace Lasallecms\Lasallecmstokenbasedlogin\Email;
 
-    /**
-     *
-     * Email handling package for the LaSalle Customer Relationship Management package.
-     *
-     * Based on the Laravel 5 Framework.
-     *
-     * Copyright (C) 2015 - 2016  The South LaSalle Trading Corporation
-     *
-     * This program is free software: you can redistribute it and/or modify
-     * it under the terms of the GNU General Public License as published by
-     * the Free Software Foundation, either version 3 of the License, or
-     * (at your option) any later version.
-     *
-     * This program is distributed in the hope that it will be useful,
-     * but WITHOUT ANY WARRANTY; without even the implied warranty of
-     * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-     * GNU General Public License for more details.
-     *
-     * You should have received a copy of the GNU General Public License
-     * along with this program.  If not, see <http://www.gnu.org/licenses/>.
-     *
-     *
-     * @package    Email handling package for the LaSalle Customer Relationship Management package
-     * @link       http://LaSalleCRM.com
-     * @copyright  (c) 2015 - 2016, The South LaSalle Trading Corporation
-     * @license    http://www.gnu.org/licenses/gpl-3.0.html
-     * @author     The South LaSalle Trading Corporation
-     * @email      info@southlasalle.com
-     *
-     */
+/**
+ *
+ * Token Based Login package for the LaSalle Content Management System, based on the Laravel 5 Framework
+ * Copyright (C) 2015 - 2016  The South LaSalle Trading Corporation
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * @package    Token Based Login package for the LaSalle Content Management System
+ * @link       http://LaSalleCMS.com
+ * @copyright  (c) 2015 - 2016, The South LaSalle Trading Corporation
+ * @license    http://www.gnu.org/licenses/gpl-3.0.html
+ * @author     The South LaSalle Trading Corporation
+ * @email      info@southlasalle.com
+ *
+ */
 
 // LaSalle Software
 use Lasallecms\Lasallecmstokenbasedlogin\Repositories\UserTokenbasedloginRepository;
@@ -43,7 +40,7 @@ use Illuminate\Support\Facades\Mail;
 
 /**
  * Class SendLoginTokenEmail
- * @package Lasallecrm\Lasallecrmemail\Logintoken
+ * @package Lasallecms\Lasallecmstokenbasedlogin\SendLoginTokenEmail
  */
 class SendLoginTokenEmail
 {
